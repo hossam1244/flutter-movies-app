6e8e494cbec3dd4a0cb06a505365594bbdc4be88
Hossam Rakha <hossamrakha0@gmail.com>
2024-03-06 19:37:12 +0200

    added a unit test for networking

---
+++ test/networking.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:sample_movies_app_flutter/core/network_info.dart';
+
+
+
+
+void main() {
+  NetworkInfoImpl? networkInfoImpl;
+  InternetConnectionChecker internetConnectionChecker;
+
+  setUp(() {
+    internetConnectionChecker = InternetConnectionChecker();
+    networkInfoImpl = NetworkInfoImpl(internetConnectionChecker);
+  });
+
+  test("hasConnection", () async {
+    final result = await networkInfoImpl?.isConnected;
+    expect(result, true);
+  });
+
+  test("hasNoConnection", () async {
+    final result = await networkInfoImpl?.isConnected;
+    expect(!result!, false);
+  });
+}
