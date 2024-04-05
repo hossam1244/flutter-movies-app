1511fbbc9aefb2361abb142b46ef016d5fe73297
Hossam Rakha <hossamrakha0@gmail.com>
2024-04-05 16:13:06 +0200

    copied the hive configuration inside initHive function

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:sample_movies_app_flutter/domain/entity/movie_entity.dart';
 import 'package:sample_movies_app_flutter/view/movies_list_page.dart';
-import 'package:sample_movies_app_flutter/service_locator.dart' as di;
+import 'package:sample_movies_app_flutter/service_locator.dart'
+    as serviceLocator;
 
 import 'domain/entity/movie_results_entity.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  initHive();
+  await serviceLocator.init();
+  runApp(const MyApp());
+}
+
+Future<void> initHive() async {
+  // Initializing Hive
   await Hive.initFlutter();
+  // Registering the adapters
   Hive.registerAdapter(MovieEntityAdapter());
   Hive.registerAdapter(ResultsAdapter());
   // Opening the box
   await Hive.openBox<MovieEntity>('moviesBox');
-  await di.init();
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
