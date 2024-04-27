7f45c5e238a12d954c2fd5f3ad9f42b95bc3543c
Hossam Rakha <hossamrakha0@gmail.com>
2024-04-27 21:49:28 +0300

    init details page

--- lib/view/movies_details/movie_details_page.dart
+++ lib/view/movies_details/movie_details_page.dart
@@ -9,9 +9,25 @@ class MoviesDetailsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Movie Details'),
       ),
-      body: const Center(
-        child: Text('Movie Details'),
+      body:  Center(
+        child: _movieDetails(),
       ),
+    );
+  }
+
+  Widget _movieDetails() {
+    return Column(
+      children: [
+        Image.network(""),
+        const SizedBox(height: 10),
+        Text('Movie Name'),
+        const SizedBox(height: 10),
+        Text('Movie Description'),
+        const SizedBox(height: 10),
+        Text('Movie Rating'),
+        const SizedBox(height: 10),
+        Text('Movie Release Date'),
+      ],
     );
   }
 }
