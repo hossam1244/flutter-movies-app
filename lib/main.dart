42f28563931230a9c38429d3185a46a1c539259f
Hossam Rakha <hossamrakha0@gmail.com>
2024-05-18 15:37:34 +0300

    added bloc observer for the main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:sample_movies_app_flutter/bloc_observer.dart';
 import 'package:sample_movies_app_flutter/domain/entity/movie_entity.dart';
 import 'package:sample_movies_app_flutter/service_locator.dart' as service_locator;
 
@@ -10,6 +12,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   initHive();
   await service_locator.init();
+  Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
 
